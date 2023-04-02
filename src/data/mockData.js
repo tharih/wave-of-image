d5a326c75b6a10c04ec882b4cd545daaa9e0bfe1
slacker1992 <suvinjavax@gmail.com>
2023-04-02 13:02:11 +0530

    updated

---
+++ src/data/mockData.js
@@ -0,0 +1,57 @@
+export const imageData = [
+    {
+      id: 0, 
+      category: "photography",
+      src: "01",
+    },{
+      id: 1, 
+      category: "retouching",
+      src: "1",
+    },{
+      id: 2, 
+      category: "photography",
+      src: "2",
+    },{
+      id: 3, 
+      category: "photography",
+      src: "02",
+    },{
+      id: 4, 
+      category: "retouching",
+      src: "3",
+    },
+    {
+      id: 5, 
+      category: "color grading",
+      src: "03",
+    }, {
+      id: 6, 
+      category: "color grading",
+      src: "04",
+    }, {
+      id: 7, 
+      category: "retouching",
+      src: "5",
+    },{
+      id: 8, 
+      category: "retouching",
+      src: "05",
+    },{
+      id: 9, 
+      category: "retouching",
+      src: "6",
+    },{
+      id: 10, 
+      category: "retouching",
+      src: "06",
+    },{
+      id: 11, 
+      category: "retouching",
+      src: "7",
+    },{
+      id: 12, 
+      category: "retouching",
+      src: "8",
+    }
+  
+  ];
