a4253469f6a370b804cae8c671cd1c37683c225d
Tharinda Dissanayake <tharindadissanayake456@gmail.com>
2023-04-26 12:24:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ import Home from "./home/Home";
 import Gallery from "./gallery/Gallery";
 import Contact from "./contact/Contact";
 import VideoPage from "./video/VideoPage";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // define a custom handler function
+    // for the contextmenu event
+    const handleContextMenu = (e) => {
+      // prevent the right-click menu from appearing
+      e.preventDefault();
+    };
+
+    // attach the event listener to
+    // the document object
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    // clean up the event listener when
+    // the component unmounts
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
   return (
     <BrowserRouter>
       <Header />
